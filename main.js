3c88b206d02b4eed91005b4c33f1bf294a3180a9
cynthia <you@example.com>
2023-09-07 23:24:38 +0100

    new commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const slackUserName = 'Cynthia'
+const slackDisplayImage = '/hero-pic.png'
+const myTrack = 'Frontend Track'
+
+// document.getElementById('datetime').innerHTML = $scope.currentDate;
+document.querySelector('.first-heading').innerHTML = slackUserName;
+document.querySelector('.first-para').innerHTML = myTrack;
+const imgElement = document.querySelector('.item'); 
+imgElement.setAttribute('alt', slackUserName);
+
+const imgSrc = document.querySelector('.item'); 
+imgElement.setAttribute('src', slackDisplayImage);
+
+const githubLink = document.querySelector('.btn'); 
+githubLink.setAttribute('href', slackUserName);
+
+const currentTime = document.querySelector('#datetime')
+
+const timeTicker = () => {
+    const currentDate = new Date().toLocaleString('en-US', {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        timeZoneName: 'long'
+      });
+    currentTime.innerHTML = currentDate;
+    return currentDate;
+  }
+  
+  setInterval(timeTicker, 1000);
